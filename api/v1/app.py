cfcd1cf56bd59594edb82b2bcd002d4093dc6d06
Khaled Mahmoud <khaled.mahmoud.23rd@gmail.com>
2024-04-27 08:35:09 +0300

    v3-01

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,51 +1,35 @@
 #!/usr/bin/python3
-""" Script that imports a Blueprint and runs Flask """
-from flask import Flask, make_response, jsonify
-from flask_cors import CORS
+"""Flask server (variable app)
+"""
+
+
+from flask import Flask, jsonify
 from models import storage
-from api.v1.views import app_views
 from os import getenv
-from flasgger import Swagger
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-app.config['SWAGGER'] = {
-    "swagger_version": "2.0",
-    "title": "Flasgger",
-    "headers": [
-        ('Access-Control-Allow-Origin', '*'),
-        ('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS"),
-        ('Access-Control-Allow-Credentials', "true"),
-    ],
-    "specs": [
-        {
-            "version": "1.0",
-            "title": "HBNB API",
-            "endpoint": 'v1_views',
-            "description": 'HBNB REST API',
-            "route": '/v1/views',
-        }
-    ]
-}
-swagger = Swagger(app)
+app.url_map.strict_slashes = False
+
 
 @app.teardown_appcontext
-def teardown_session(exception):
-    """ Closes storage session """
+def downtear(self):
+    '''Status of your API'''
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """ Returns JSON response with 404 status """
-    return make_response(jsonify({"error": "Not found"}), 404)
+def page_not_found(error):
+    '''return render_template'''
+    return jsonify('error='Not found'), 404
 
 
-if __name__ == '__main__':
-    HBNB_API_HOST = getenv('HBNB_API_HOST')
-    HBNB_API_PORT = getenv('HBNB_API_PORT')
-
-    host = '0.0.0.0' if not HBNB_API_HOST else HBNB_API_HOST
-    port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
